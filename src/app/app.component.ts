45ad760f93324306539c0b0f6ea2a6b34a439520
aryandh.a.w <aryandh.a.w@gmail.com>
2024-07-15 16:49:07 +0700

    list employee page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './employee.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-management-frontend';
+  displayedColumns: string[] = [
+    "userName",
+    "firstName",
+    "lastName",
+    "email",
+    "birthDate",
+    "basicSalary",
+    "status",
+    "group",
+    "description",
+    "action",
+  ];
+
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  // dependency injection
+  constructor(
+    private dialog: MatDialog,
+    private empService: EmployeeService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  openAddEditEmployeeDialog() {
+    const dialogRef = this.dialog.open(EmpAddEditComponent, {
+      width: '600px'
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  }
+
+  getEmployeeList() {
+    this.empService.getEmployeeList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        console.log(res);
+      },
+    });
+  }
+
+  // for searching employee with firstname, lastname, gender, etc
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: number) {
+    let confirm = window.confirm("Are you sure you want to delete this employee?");
+    if(confirm) {
+      this.empService.deleteEmployee(id).subscribe({
+        next: (res) => {
+          alert("Employee deleted!");
+          this.getEmployeeList();
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this.dialog.open(EmpAddEditComponent, {
+      width: '600px',
+      data,
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  }
+
+
 }
